aa46db63e1c8bf5377fff897f457d15bb449a0c4
kamillaannakovacs <145448165+kamillaannakovacs@users.noreply.github.com>
2023-10-16 10:54:41 +0200

    Update data_transformations.r

--- functions/data_transformations.r
+++ functions/data_transformations.r
@@ -21,3 +21,32 @@ transform_metadata_to_df <- function(stations_metadata) {
 
   return(stations_df)
 }
+
+
+# Problem 4a
+
+library(anytime)
+
+# 'to_iso8601' converts a date-time object to ISO 8601 format, applying an offset in days.
+# 
+# @param datetime A date-time object.
+# @param offset_days The number of days to add to 'datetime' (can be negative for subtraction).
+# @return A string with the date-time in ISO 8601 format.
+to_iso8601 <- function(datetime, offset_days) {
+  # Apply the offset
+  offset_datetime <- datetime + days(offset_days)
+  
+  # Convert to ISO 8601 format with "Z" (Zulu time zone, i.e., UTC)
+  iso_datetime <- anytime::iso8601(offset_datetime, tz = "UTC")
+  
+  return(iso_datetime)
+}
+
+
+source('functions/data_transformations.r')
+
+print(to_iso8601(as_datetime("2016-09-01 10:11:12"), 0))
+# Should print: [1] "2016-09-01T10:11:12Z"
+
+print(to_iso8601(as_datetime("2016-09-01 10:11:12"), -4))
+# Should print: [1] "2016-08-28T10:11:12Z"

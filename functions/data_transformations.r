08cc1f51f9ace8a6ceaa0c6d022831706fa84223
kamillaannakovacs <145448165+kamillaannakovacs@users.noreply.github.com>
2023-10-16 10:10:19 +0200

    Update data_transformations.r

--- functions/data_transformations.r
+++ functions/data_transformations.r
@@ -1,1 +1,3 @@
-
+# transform_metadata_to_df.
+# The function transform_metadata_to_df should complete the transformation of stations_metadata to a data frame that 
+# looks similar to the example rows below:
